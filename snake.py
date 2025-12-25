67f60153ac46e9d30addabb49e1d6c932afc436a
mpmp <vevevek10@gmail.com>
2025-12-25 21:46:25 +0100

    progress

---
+++ snake.py
@@ -0,0 +1,65 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00ffb8"
+FOOD_COLOR = "#e10000"
+BACKGROUND_COLOR = "#000000"
+
+
+class Snake:
+    pass
+
+class Food:
+
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snake Game")
+
+window.update()
+
+score = 0
+direction = "down"
+label = Label(window, text="Score:{}".format(score), font=("Arial", 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, width=GAME_WIDTH, height=GAME_HEIGHT)
+canvas.pack()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width // 2) - (window_width // 2))
+y = int((screen_height // 2) - (window_height // 2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+window.mainloop()
