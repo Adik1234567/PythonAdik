a6c82abf8bf0c9f8d8151d188624866570324641
Adrian <mpmp@MPMP--MacBook-Air.local>
2025-12-25 20:32:40 +0100

    Initial commit

---
+++ .venv/bin/snake.py
@@ -0,0 +1,55 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00ffb8"
+FOOD_COLOR = "#e10000"
+BACKGROUND_COLOR = "#000000"
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snake Game")
+window.resizable(width=False, height=False)
+window.update()
+
+score = 0
+direction = "down"
+label = Label(window, text="Score:{}".format(score), font=("Arial", 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, width=GAME_WIDTH, height=GAME_HEIGHT)
+canvas.pack()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int(screen_width / 2) - (window_width / 2)
+y = int(screen_height / 2) - (window_height / 2)
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+window.mainloop()
